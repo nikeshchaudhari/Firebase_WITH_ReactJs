aa099dfee95cd057d63b58e20ffabd2665c086f0
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-04-04 13:43:10 +0545

    updatating

---
+++ src/component/Dashboard.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import "../style.css";
+const Dashboard = () => {
+  return (
+    <div className="wrapper-dashboard">
+      <div className="side-wrapper">
+       <Link to = '/addstudent'style={{
+        color:'white', display:'block',padding:'12px'
+       }}>Add Student</Link>
+       <Link to = '/studentlist'style={{
+        color:'white', display:'block',padding:'12px'
+       }}> Student List</Link>
+      </div>
+      <div> 
+       <Outlet/>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
