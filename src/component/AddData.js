36e296556c44afe4ccb578caae9112a95f45ac5d
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-04-04 13:41:50 +0545

    updatating

--- src/component/AddData.js
+++ src/component/AddData.js
@@ -1,19 +1,20 @@
 import React from 'react'
-import { getDatabase,ref,set } from 'firebase/database'
 import {app} from '../Firebase'
+import { getDatabase,ref,set } from 'firebase/database'
 const AddData = () => {
 const addData =(userId,name,phone)=>{
-    const db =getDatabase(app);
-    set(ref(db,'student/'+userId),{
-        studentName: name,
-        phone:phone
-
-    })
+const db= getDatabase(app)
+set(ref(db,'student/'+userId),{
+  studntName:name,
+  phone:phone
+})
 }
+
+
   return (
     <div>
       <h1>Add Data DEMO</h1>
-      <button onClick={()=>{addData(123,'Nikesh',1589148)}}>Add Demo Data</button>
+      <button onClick={()=>{addData(456,"Aman",457788)}}>Add Demo Data</button>
     </div>
   )
 }
