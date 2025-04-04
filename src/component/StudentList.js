91dfe14b9dd21e3819bd5ab21553e2d402c1ee16
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-04-04 23:44:00 +0545

    updatating

--- src/component/StudentList.js
+++ src/component/StudentList.js
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { getDatabase, onValue,ref } from "firebase/database";
+import { getDatabase, onValue, ref } from "firebase/database";
 import { app } from "../Firebase";
 
 const StudentList = () => {
-    const [studentData,setStudentData]=useState();
+  const [studentData, setStudentData] = useState(null);
   useEffect(() => {
     const db = getDatabase(app);
-    const studentRef=ref(db,'student')
-    onValue(studentRef,(snapshot)=>{
-        const data = snapshot.val()
-        setStudentData(data);
-    })
+    const studentRef = ref(db, "student");
+    onValue(studentRef, (snapshot) => {
+      const data = snapshot.val();
+      setStudentData(data);
+    });
   }, []);
   return (
     <div>
       <h1>Stduent List</h1>
       {studentData && (
         <div>
-            {Object.entries(studentData)}
+          {Object.entries(studentData).map(([key, value]) => {
+            return (
+              <div key={key}>
+                <p>{value.studentName} {value.phone}</p>
+              </div>
+            );
+          })}
         </div>
       )}
     </div>
