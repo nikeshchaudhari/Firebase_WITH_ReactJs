8d51b87a125021bd5ba2698ef3873062cf02edf8
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-04-04 13:41:29 +0545

    updatating

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import AddData from './component/AddData'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Dashboard from './component/Dashboard'
+import AddStudent from './component/AddStudent'
+import StudentList from './component/StudentList'
+const myRouter =createBrowserRouter([
+  {path:"",Component:Dashboard,children:[
+    {path:'',Component:AddStudent},
+    {path:'addstudent',Component:AddStudent},
+    {path:'studentlist',Component:StudentList},
+  ]},
+  
+])
 const App = () => {
   return (
     <div>
-      <AddData/>
+      <RouterProvider router={myRouter} ></RouterProvider>
     </div>
   )
 }
