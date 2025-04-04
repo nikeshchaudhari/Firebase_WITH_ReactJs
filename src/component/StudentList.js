f5b47ecdff9d7c94109b0cec09543cc38a95a8ac
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-04-04 23:33:33 +0545

    updatating

--- src/component/StudentList.js
+++ src/component/StudentList.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getDatabase, onValue,ref } from "firebase/database";
+import { app } from "../Firebase";
 
 const StudentList = () => {
+    const [studentData,setStudentData]=useState();
+  useEffect(() => {
+    const db = getDatabase(app);
+    const studentRef=ref(db,'student')
+    onValue(studentRef,(snapshot)=>{
+        const data = snapshot.val()
+        setStudentData(data);
+    })
+  }, []);
   return (
     <div>
       <h1>Stduent List</h1>
+      {studentData && (
+        <div>
+            {Object.entries(studentData)}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default StudentList
+export default StudentList;
